ef15cd564ee1e8659a2c1e7ac095d29404616750
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-11-16 00:03:30 +1000

    Added isEmpty for state.

--- Sources/MachineViews/view_models/StateViewModel.swift
+++ Sources/MachineViews/view_models/StateViewModel.swift
@@ -113,6 +113,12 @@ public class StateViewModel: ObservableObject {
         machine[keyPath: path.path].transitions(in: machine).count == 0
     }
     
+    var isEmpty: Bool {
+        actions.reduce(true) {
+            $0 && $1.1.isEmpty
+        }
+    }
+    
     public init(machine: Machine, path: Attributes.Path<Machine, Machines.State>, location: CGPoint = CGPoint(x: 75, y: 100), width: CGFloat = 75.0, height: CGFloat = 100.0, expanded: Bool = false) {
         self.machine = machine
         self.path = path
