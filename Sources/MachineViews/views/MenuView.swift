9b3347c045934bc106a35876a63b20e2d0ebb4e1
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-11-20 05:46:29 +1000

    Added some basic buttons.

--- Sources/MachineViews/views/MenuView.swift
+++ Sources/MachineViews/views/MenuView.swift
@@ -19,8 +19,21 @@ struct MenuView: View {
     
     var body: some View {
         HStack {
+            Button(action: {  }) {
+                // New Machine
+                Image(systemName: "folder.fill.badge.plus")
+            }
             Button(action: { openDialogue = true }) {
+                // Open Machine
                 Image(systemName: "folder.fill")
+            }
+            Button(action: {  }) {
+                // Save Machine
+                Image(systemName: "folder.circle")
+            }
+            Button(action: {  }) {
+                // Save-As
+                Image(systemName: "folder.circle.fill")
             }
         }
     }
