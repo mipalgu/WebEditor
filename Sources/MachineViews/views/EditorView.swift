d46d2228ba89fd901cf84e14cf1ed0c7aff95a69
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-11-20 05:00:15 +1000

    New view for editor.

---
+++ Sources/MachineViews/views/EditorView.swift
@@ -0,0 +1,23 @@
+//
+//  EditorView.swift
+//  
+//
+//  Created by Morgan McColl on 20/11/20.
+//
+
+#if canImport(TokamakShim)
+import TokamakShim
+#else
+import SwiftUI
+#endif
+import Machines
+import Attributes
+
+struct EditorView: View {
+    
+    
+    
+    var body: some View {
+        EmptyView()
+    }
+}
