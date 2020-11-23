e1b7b4ed0010ab8b7c2d8ddf56cdfee9f7b16fdb
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2020-11-23 17:22:21 +1000

    Added grid.

--- Sources/MachineViews/views/MachineView.swift
+++ Sources/MachineViews/views/MachineView.swift
@@ -43,6 +43,34 @@ public struct MachineView: View {
                 }
             }
         }
+        .background(
+            GeometryReader { reader in
+                ZStack {
+                    HStack {
+                        ForEach(Array(stride(from: -reader.size.width / 2.0 + viewModel.gridWidth, to: reader.size.width / 2.0, by: viewModel.gridWidth)), id: \.self) {
+                            Divider()
+                                .coordinateSpace(name: "MAIN_VIEW")
+                                .position(x: $0, y: reader.size.height / 2.0)
+                                .frame(width: 2.0, height: reader.size.height)
+                                .foregroundColor(config.stateColour)
+                        }
+                    }
+                    VStack {
+                        ForEach(
+                            Array(stride(from: -reader.size.height / 2.0 + viewModel.gridHeight, to: reader.size.height / 2.0, by: viewModel.gridHeight)),
+                            id: \.self
+                        ) {
+                            Divider()
+                                .coordinateSpace(name: "MAIN_VIEW")
+                                .position(x: reader.size.width / 2.0, y: $0)
+                                .frame(width: reader.size.width, height: 2.0)
+                                .foregroundColor(config.stateColour)
+                        }
+                    }
+                }
+            }
+            .clipped()
+        )
     }
 }
 
