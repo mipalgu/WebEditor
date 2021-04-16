b63762b50c3dc295a3680aab2e990e3cb57c9c1c
Callum McColl <callum.mccoll1@gmail.com>
2021-04-16 17:07:54 +1000

    Only use one width and height binding in StateView

--- Sources/MachineViews/views/StateView.swift
+++ Sources/MachineViews/views/StateView.swift
@@ -20,28 +20,61 @@ struct StateView: View {
     
     @Binding var machine: Machine
     let path: Attributes.Path<Machine, Machines.State>
+    @Binding var width: CGFloat
+    @Binding var height: CGFloat
     
     @State var collapsedActions: [String: Bool] = [:]
+    @State var expanded: Bool
     
-    @State var expandedWidth: CGFloat = 300
-    @State var expandedHeight: CGFloat = 200
+    @State var collapsedWidth: CGFloat
+    @State var collapsedHeight: CGFloat
+    @State var expandedWidth: CGFloat
+    @State var expandedHeight: CGFloat
     
-    @State var collapsedWidth: CGFloat = 150
-    @State var collapsedHeight: CGFloat = 200
-    
-    @State var expanded: Bool = false
+    init(machine: Binding<Machine>, path: Attributes.Path<Machine, Machines.State>, width: Binding<CGFloat> = .constant(300), height: Binding<CGFloat> = .constant(200), expanded: Bool = false) {
+        self._machine = machine
+        self.path = path
+        self._width = width
+        self._height = height
+        self._expanded = State(initialValue: expanded)
+        if expanded {
+            self._collapsedWidth = State(initialValue: 150)
+            self._collapsedHeight = State(initialValue: 200)
+            self._expandedWidth = State(initialValue: width.wrappedValue)
+            self._expandedHeight = State(initialValue: height.wrappedValue)
+        } else {
+            self._collapsedWidth = State(initialValue: width.wrappedValue)
+            self._collapsedHeight = State(initialValue: height.wrappedValue)
+            self._expandedWidth = State(initialValue: 300)
+            self._expandedHeight = State(initialValue: 200)
+        }
+    }
     
     @EnvironmentObject var config: Config
     
     var body: some View {
-        if expanded {
-            StateExpandedView(root: $machine, path: path, collapsedActions: $collapsedActions) {
-                StateTitleView(machine: $machine, path: path.name, expanded: $expanded)
-            }.frame(width: max(expandedWidth, 300), height: max(expandedHeight, 200))
-        } else {
-            StateCollapsedView {
-                StateTitleView(machine: $machine, path: path.name, expanded: $expanded)
-            }.frame(width: max(collapsedWidth, 75), height: max(collapsedHeight, 100))
+        Group {
+            if expanded {
+                StateExpandedView(root: $machine, path: path, collapsedActions: $collapsedActions) {
+                    StateTitleView(machine: $machine, path: path.name, expanded: $expanded)
+                }.frame(width: max(width, 300), height: max(height, 200))
+            } else {
+                StateCollapsedView {
+                    StateTitleView(machine: $machine, path: path.name, expanded: $expanded)
+                }.frame(width: max(width, 75), height: max(height, 100))
+            }
+        }.onChange(of: expanded) { newValue in
+            if newValue {
+                collapsedWidth = width
+                collapsedHeight = height
+                width = expandedWidth
+                height = expandedHeight
+            } else {
+                expandedWidth = width
+                expandedHeight = height
+                width = collapsedWidth
+                height = collapsedHeight
+            }
         }
     }
 }
