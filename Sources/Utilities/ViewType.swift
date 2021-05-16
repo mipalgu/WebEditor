69c2f2486c4890ab1087accc8eb834b3cbeb410a
Callum McColl <callum.mccoll1@gmail.com>
2021-05-16 11:58:52 +1000

    Created convenience properties in ViewType

--- Sources/Utilities/ViewType.swift
+++ Sources/Utilities/ViewType.swift
@@ -8,6 +8,32 @@
 import Foundation
 
 public enum ViewType: Hashable {
+    
+    public var stateIndex: Int {
+        switch self {
+        case .state(let stateIndex), .transition(let stateIndex, _):
+            return stateIndex
+        }
+    }
+    
+    public var isState: Bool {
+        switch self {
+        case .state:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var isTransition: Bool {
+        switch self {
+        case .transition:
+            return true
+        case .state:
+            return false
+        }
+    }
+    
     case state(stateIndex: Int)
     case transition(stateIndex: Int, transitionIndex: Int)
 }
